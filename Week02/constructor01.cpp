2f8d1b9ad65b643a88eb7ce6546d2a97b3c64d41
Sanjid Hasan <155468281+Sanjidh090@users.noreply.github.com>
2024-12-08 16:16:22 +0600

    Create constructor01.cpp

---
+++ Week02/constructor01.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+class Point {
+private:
+    int x, y;
+
+public:
+    Point(int a, int b) {
+        x = a;
+        y = b;
+    }
+
+    int getX() {
+        return x;
+    }
+
+    int getY() {
+        return y;
+    }
+};
+
+float distance(Point p1, Point p2) {
+    float d;
+    d = sqrt(pow(p1.getX() - p2.getX(), 2) + pow(p1.getY() - p2.getY(), 2));
+    return d;
+}
+
+int main() {
+    Point p1(2, 3), p2(5, 5);
+
+    cout << "Distance: " << distance(p1, p2) << endl;
+
+    return 0;
+}
