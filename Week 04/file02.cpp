dc1ef71a0ed3236e7fdd89be037498cac3603809
Sanjid Hasan <155468281+Sanjidh090@users.noreply.github.com>
2024-12-29 20:23:18 +0600

    Create file02.cpp

---
+++ Week 04/file02.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    // string line;
+    // ifstream read;
+    // read.open("Student.txt"); 
+    // while(getline(read,line)){cout << line << endl;}
+    // getline(read,line);
+    // cout << line <<endl;
+    // getline(read,line);
+    // cout << line <<endl;
+
+string line;
+ifstream read;
+read.open("Student.txt");
+getline(read,line);
+cout<< line << endl;
+read.close();
+cout<< line << endl;
+
+    return 0;
+}
