48797d2f486f64f5fe17ffa6dadc215e2cc82c0e
Sanjid Hasan <155468281+Sanjidh090@users.noreply.github.com>
2025-01-12 19:06:49 +0600

    Add files via upload

---
+++ Week 06/trav.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[] = {10,20,30,40,50};
+    cout << arr[3] << endl;
+    for(int i = 0; i <= 4; i++)
+    {
+        cout << i << " th element and value "<< " " << arr[i] << endl;
+    }
+    return 0;
+}
